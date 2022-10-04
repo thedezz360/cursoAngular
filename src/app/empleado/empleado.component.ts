08083604e1687a0348ec16a3aa44387cb7fedc31
thedezz360 <thedezz360@gmail.com>
2022-10-04 18:47:49 +0200

    terminado event binding, comienza two way binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -37,7 +37,25 @@ export class EmpleadoComponent implements OnInit {
     return this.edad;
   }
 
-  
+  //para usar con el event binding
+  textoDeRegistro="No hay nadie registrado";
+  //event se crea automaticamente al utilizar cualquier evento , simplemente lo
+  //estamos capturando
+  setUsuarioRegistrado(evento:Event){
+    //alert("El usuario se acaba de registrar");
+
+    //this.textoDeRegistro="El usuario se acaba de registrar";
+    //hacemos un casting del avento a htmlinputElement,
+    //aunque el evento provenia de un htmlinputElement
+    if((<HTMLInputElement>evento.target).value=="si"){
+      this.textoDeRegistro="El usuario se acaba de registrar";
+    }else{
+      this.textoDeRegistro="No hay nadie registrado";
+    }
+  }
+
+
+
   constructor() { }
 
   ngOnInit(): void {
